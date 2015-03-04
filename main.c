6435b16358dc362ba6a66535074f54542bd02e1d
xandout <turnerdistributed@gmail.com>
2015-03-04 16:31:32 -0500

    ARGGGHHHHHH WHY CAN'T I READ FILES???

--- main.c
+++ main.c
@@ -1,30 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "md5.h"
+#include <errno.h>
+
+
+void parseFile(const char *path){
+
+    unsigned char buffer[13];
+    FILE *ptr = fopen(path,"rb");
+    if(ptr == NULL){
+        printf("The error is - %s\n", strerror(errno));
+    }
+
+//    char md5string[33];
+//        md5_state_t state;
+//        md5_byte_t digest[16];
+//        md5_init(&state);
+//        md5_append(&state, (md5_byte_t *)buffer, strlen(buffer));
+//        md5_finish(&state, digest);
+//        md5_string(digest, md5string);
+//        puts(md5string);
+
+}
 
 int main(int argc, char* argv[])
 {
-    if(argc < 2)
-    {
-        printf("\nUsage: %s <string to hash>", argv[0]);
-        return 0;
-    }
-    else
-    {
-
-        char md5string[33];
-        md5_state_t state;
-        md5_byte_t digest[16];
-        char hex_output[16*2 + 1];
-        md5_init(&state);
-        md5_append(&state, (md5_byte_t *)argv[1], strlen(argv[1]));
-        md5_finish(&state, digest);
-        md5_string(digest, md5string);
-        puts(md5string);
-
-
-        return 0;
-
-    }
+    parseFile("MCPR.exe");
+//    if(argc < 2)
+//    {
+//        printf("\nUsage: %s <string to hash>", argv[0]);
+//        return 0;
+//    }
+//    else if(!strcmp(argv[1], "-file"))
+//    {
+//        parseFile(argv[2]);
+//    }
+//    else
+//    {
+//
+//        char md5string[33];
+//        md5_state_t state;
+//        md5_byte_t digest[16];
+//        md5_init(&state);
+//        md5_append(&state, (md5_byte_t *)argv[1], strlen(argv[1]));
+//        md5_finish(&state, digest);
+//        md5_string(digest, md5string);
+//        puts(md5string);
+//
+//
+//        return 0;
+//
+//    }
 
 }
